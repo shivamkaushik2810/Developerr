6cc7a8a262f4f6a852b3df4ccb99118ec7c29214
Shivam Kaushik <shivamkaushik80875@gmail.com>
2025-09-19 21:56:57 +0530

    19.09.25

---
+++ DSA/ArrayList/PushAtBottomRec.java
@@ -0,0 +1,13 @@
+import java.util.*;
+package C.Users.Shiva.DEVELOPER.DSA.Stack1;
+class Push{
+    Stack st =new Stack<>();
+    
+}
+
+public class PushAtBottomRec {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ DSA/Stack/StUsingLinkList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Linklist{
+    Stack<Integer> st = new Stack<>();
+
+    
+   
+
+}
+
+public class StUsingLinkList {
+    public static void main(String[] args) {
+        
+    }
+	
+}
