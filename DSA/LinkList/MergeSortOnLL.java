c09d9147acde6de14cc7884f1d1397bac840a683
Shivam Kaushik <shivamkaushik80875@gmail.com>
2025-11-04 22:29:12 +0530

    ll

---
+++ DSA/LinkList/MergeSortOnLL.java
@@ -0,0 +1,137 @@
+ class LinkedList {
+ public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public  Node head;
+    public  Node tail;
+    
+
+
+    //add in front ll
+    public void addFirst(int data){//0(1) constant time
+        //1create new node
+        Node newNode=new Node(data);
+        
+        //check if ll is empty
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+         //2new node next=head
+        newNode.next=head;//link one node to another
+        //3head=newNode
+        head=newNode;
+    }
+    //add in last ll
+    public void addLast(int data){//0(1) constant time
+        //1create new node
+        Node newNode=new Node(data);
+        
+        //check if ll is empty
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        //2tail.next=newNode
+        tail.next=newNode;
+        //3tail=newNode
+        tail=newNode;
+    }
+    //print ll
+    public void printLL(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    //step 1 find mid of ll
+    private Node getMid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    //step 2 -> find 
+    //  a-> left half
+    //  b-> right half 
+    private Node merge(Node head1,Node head2){
+        Node mergedLL=new Node(-1);
+        Node temp=mergedLL;
+
+        while(head1!=null && head2!=null){
+            if(head1.data<=head2.data){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+        while(head1!=null){
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while(head2!=null){
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return mergedLL.next;
+    }
+    //step 3-> merge together left and right
+    public Node mergeSort(Node head){
+        //base case
+        if(head==null || head.next==null){
+            return head;
+        }
+        //find mid
+        Node mid=getMid(head);
+        //left and right ms
+        Node rightHead=mid.next;
+        mid.next=null;
+        Node newLeft=mergeSort(head);
+        Node newRight=mergeSort(rightHead);
+
+        //merge
+        return merge(newLeft,newRight);
+    }
+        
+}
+    
+
+
+public class MergeSortOnLL {
+    public static void main(String[] args) {
+        LinkedList ll=new LinkedList();
+        ll.addLast(4);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.addLast(3);
+        ll.addLast(5);
+        ll.printLL();
+        ll.head=ll.mergeSort(ll.head);
+        ll.printLL();
+       
+        
+    }
+    
+}
