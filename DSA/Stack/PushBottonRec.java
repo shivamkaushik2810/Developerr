edda212cc135d20a8cc1f457a98f6aecbb41aee7
Shivam Kaushik <shivamkaushik80875@gmail.com>
2026-01-19 06:26:10 +0530

    Push at Bottom using recursion

--- DSA/Stack/PushBottonRec.java
+++ DSA/Stack/PushBottonRec.java
@@ -1,3 +1,7 @@
+
+
+// Push an element at bottom of stack using recursion
+
 import java.util.Stack;
 class StackA{
     public void pushAtBottam(Stack<Integer> s, int data){
@@ -12,15 +16,20 @@ class StackA{
 }
 public class PushBottonRec{
     
-    public static void main(String[] args) {
-        Stack<Integer> s=new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
-    
-            
-        }
-        }
+    // Modified main method for demonstration
+public static void main(String[] args) {
+    Stack<Integer> s=new Stack<>();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    // CALL THE FUNCTION
+    StackA sa = new StackA();
+    sa.pushAtBottam(s, 99); // Pushing 99 to the bottom
+
+    while (!s.isEmpty()) {
+        System.out.println(s.pop());
+    }
+}
+
     }
