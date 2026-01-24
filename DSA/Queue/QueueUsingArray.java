e909ee7c26e951092b65ab7daf953bd84848c104
Shivam Kaushik <shivamkaushik80875@gmail.com>
2026-01-24 07:42:43 +0530

    Queue using array

---
+++ DSA/Queue/QueueUsingArray.java
@@ -0,0 +1,68 @@
+
+
+public class QueueUsingArray {
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+        Queue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+        }
+        public boolean isEmpty(){
+            return rear==-1;
+        }
+
+        //add
+        public void add(int data){
+            if(rear==size-1){
+                System.out.println("Queue is full");
+                return;
+            }
+            rear++;
+            arr[rear]=data;
+        }
+
+
+        //remove
+        public int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front=arr[0];
+            //shift all the elements to left
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+
+        //peek
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+
+
+    }
+    public static void main(String[] args) {
+        Queue q=new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+    
+}
